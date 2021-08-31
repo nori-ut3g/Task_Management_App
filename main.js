630f72017fafe4fcd94d43df9fb27aaaff29c09c
nori-ut3g <akinori.nkd@gmail.com>
2021-08-31 16:46:06 +0900

    最低限のclass追加

---
+++ main.js
@@ -0,0 +1,62 @@
+class Task{
+    constructor(sectionID) {
+        this.sectionID = sectionID;//int
+        this.taskName = "";//string
+        this.contens = "";//string
+        this.isFavorite = false;//bool
+    }
+
+    setContents(contents){
+
+    }
+
+
+
+
+}
+// Vue.component("task", {
+//
+// })
+
+class Section{
+    constructor(sectionID, sectionName){
+        this.sectionID = sectionID;//int
+        this.sectionName = sectionName;//string
+        this.tasks = [];//[Task]
+    }
+
+    createTask(){
+        this.tasks.push(new Task(this.sectionID));
+    }
+
+}
+
+class SectionList{
+    constructor() {
+        this.numberingID = 0;
+        this.sections = [];
+    }
+
+    createSection(sectionName){
+        this.numberingID++;
+        this.sections.push(new Section(this.numberingID, sectionName));
+    }
+}
+
+// Vue.component('task', {
+//     props: ['sectionid'],
+//
+//     template: `
+//     <div class="card m-2" style="width: 18rem;">
+//         <div class="card-body">
+//         </div>
+//     </div>`
+// });
+// Vue.component('section', {
+//     props: ["itemcard"],
+//
+//     template: `
+//     <div class="card m-2" style="width: 18rem;">
+//
+//     </div>`
+// });
